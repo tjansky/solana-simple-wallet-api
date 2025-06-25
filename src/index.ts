2efe95df26530235ad9221f230759587fa93e6b0
TJANSKY\tomis <tomislav.jansky@gmail.com>
2025-06-25 18:04:40 +0200

    Implemented Bull job dashboard

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { ExpressAdapter } from "@bull-board/express";
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { walletQueue } from "./queues/walletQueue";
 import dotenv from "dotenv";
 import walletRoutes from "./routes/walletRoutes";
 // import './cron/scheduler' - commented so cron does not auto start
@@ -7,6 +11,16 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath("/admin/queues");
+
+createBullBoard({
+  queues: [new BullMQAdapter(walletQueue)],
+  serverAdapter,
+});
+
+app.use("/admin/queues", serverAdapter.getRouter());
 
 app.use("/wallets", walletRoutes);
 
